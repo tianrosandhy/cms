23e7d6d55207f20d7fc1243295060a72c9ecd058
Christian <tianrosandhy@gmail.com>
2021-09-16 00:34:13 +0800

    autocrud:structure

--- app/Core/Configs/module-setting.php
+++ app/Core/Configs/module-setting.php
@@ -3,5 +3,6 @@ return [
 	'stubs' => [
 		'process' => 'app/Core/Stubs/Process/ExampleProcess.stub',
 		'presenter' => 'app/Core/Stubs/Presenters/ExamplePresenter.stub',
+		'structure' => 'app/Core/Stubs/Structure/ExampleStructure.stub',
 	]
 ];
