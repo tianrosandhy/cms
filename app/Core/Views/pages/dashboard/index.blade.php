6db375b657f2e041e2cc603fed465dfa8f29cad3
Christian <tianrosandhy@gmail.com>
2022-02-07 19:07:03 +0800

    composer : update dbal version, remove unused dependency

--- app/Core/Views/pages/dashboard/index.blade.php
+++ app/Core/Views/pages/dashboard/index.blade.php
@@ -1,7 +1,5 @@
 @extends ('core::layouts.master')
 
-{{-- You can delete this block if you dont wanna use Google Analytic Dashboard Component --}}
-@if(config('analytics.view_id'))
-    @include ('core::components.analytic.dashboard')
-@endif
-{{-- end analytic block --}}
+@section ('content')
+    {{-- Here is your dashboard content --}}
+@stop
